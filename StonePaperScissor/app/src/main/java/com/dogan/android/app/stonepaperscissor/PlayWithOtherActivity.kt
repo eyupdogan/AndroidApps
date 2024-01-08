78cfd66a81f7a28a3a1b4751412a14e2155a2507
Eyup Dogan <eyodohan@gmail.com>
2024-01-08 15:52:54 +0300

    update

--- StonePaperScissor/app/src/main/java/com/dogan/android/app/stonepaperscissor/PlayWithOtherActivity.kt
+++ StonePaperScissor/app/src/main/java/com/dogan/android/app/stonepaperscissor/PlayWithOtherActivity.kt
@@ -17,6 +17,13 @@ class PlayWithOtherActivity : AppCompatActivity()
     private var player1Ready:Boolean = false
     private var player2Ready:Boolean = false
     private var allowPlaying:Boolean = true
+
+    private lateinit var selectionP1:String
+    private lateinit var selectionP2:String
+
+    private var scoreP1 = 0
+    private var scoreP2 = 0
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -24,12 +31,33 @@ class PlayWithOtherActivity : AppCompatActivity()
         setContentView(binding?.root)
 
         binding?.btnP2Rock?.setOnClickListener {
-            playAnimation()
+            onPlayP2("rock")
+        }
+        binding?.btnP2Paper?.setOnClickListener {
+            onPlayP2("paper")
+        }
+        binding?.btnP2Scissor?.setOnClickListener {
+            onPlayP2("scissor")
+        }
+
+        binding?.btnP1Rock?.setOnClickListener {
+            onPlayP1("rock")
+        }
+        binding?.btnP1Paper?.setOnClickListener {
+            onPlayP1("paper")
+        }
+        binding?.btnP1Scissor?.setOnClickListener {
+            onPlayP1("scissor")
         }
     }
 
     private fun playAnimation()
     {
+        binding?.ivIconP1?.setImageResource(0)
+        binding?.ivIconP2?.setImageResource(0)
+        binding?.tvP1Status?.text = ""
+        binding?.tvP2Status?.text = ""
+
         binding?.ivIconP1?.setBackgroundResource(R.drawable.animation_rps)
         animation1 = binding?.ivIconP1?.background as AnimationDrawable
 
@@ -47,7 +75,103 @@ class PlayWithOtherActivity : AppCompatActivity()
             {
                 animation1?.stop()
                 animation2?.stop()
+                allowPlaying = true
+                player1Ready = false
+                player2Ready = false
+                binding?.ivIconP1?.setBackgroundResource(0)
+                binding?.ivIconP2?.setBackgroundResource(0)
+                setSelectedIcon()
+                setScore()
             }
         }.start()
+    }
+
+    private fun onPlayP1(selection:String)
+    {
+        if (allowPlaying){
+            binding?.ivIconP1?.setImageResource(R.drawable.check)
+            binding?.tvP1Status?.text = "Ready"
+            selectionP1 = selection
+            player1Ready = true
+            if (player2Ready){
+                allowPlaying = false
+                playAnimation()
+            }
+        }
+    }
+
+    private fun onPlayP2(selection:String)
+    {
+        if (allowPlaying){
+            binding?.ivIconP2?.setImageResource(R.drawable.check)
+            binding?.tvP2Status?.text = "Ready"
+            selectionP2 = selection
+            player2Ready = true
+            if (player1Ready){
+                allowPlaying = false
+                playAnimation()
+            }
+        }
+    }
+
+    private fun setSelectedIcon()
+    {
+        when(selectionP1){
+            "rock" -> binding?.ivIconP1?.setImageResource(R.drawable.rock)
+            "paper" -> binding?.ivIconP1?.setImageResource(R.drawable.paper)
+            "scissor" -> binding?.ivIconP1?.setImageResource(R.drawable.scissor)
+        }
+
+        when(selectionP2){
+            "rock" -> binding?.ivIconP2?.setImageResource(R.drawable.rock)
+            "paper" -> binding?.ivIconP2?.setImageResource(R.drawable.paper)
+            "scissor" -> binding?.ivIconP2?.setImageResource(R.drawable.scissor)
+        }
+    }
+
+    private fun getResult():String
+    {
+        return if (selectionP1 == selectionP2)
+                    "tie"
+        else if (selectionP1 == "rock" && selectionP2 == "scissor" ||
+            selectionP1 == "paper" && selectionP2 == "rock" ||
+            selectionP1 == "scissor" && selectionP2 == "paper")
+            "P1"
+        else
+            "P2"
+    }
+
+    private fun setScore()
+    {
+        if (getResult()=="tie")
+        {
+            binding?.tvP1Status?.text = "Tie"
+            binding?.tvP2Status?.text = "Tie"
+        }else if (getResult()=="P1"){
+            binding?.tvP1Status?.text = "Win"
+            binding?.tvP2Status?.text = "Lose"
+            scoreP1++
+            when(scoreP1){
+                1->binding?.ivP1Star1?.setImageResource(R.drawable.star)
+                2->binding?.ivP1Star2?.setImageResource(R.drawable.star)
+                3->binding?.ivP1Star3?.setImageResource(R.drawable.star)
+            }
+        }else{
+            binding?.tvP1Status?.text = "Lose"
+            binding?.tvP2Status?.text = "Win"
+            scoreP2++
+            when(scoreP2){
+                1->binding?.ivP2Star1?.setImageResource(R.drawable.star)
+                2->binding?.ivP2Star2?.setImageResource(R.drawable.star)
+                3->binding?.ivP2Star3?.setImageResource(R.drawable.star)
+            }
+        }
+    }
+
+    override fun onDestroy()
+    {
+        super.onDestroy()
+        binding = null
+        setTime = null
     }
 }
